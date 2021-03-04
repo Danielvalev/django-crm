9f6e8ac98218fb7c42d4c796f1442317b525fbfe
Daniel Valev <danielvalev89@gmail.com>
2021-03-04 16:51:21 +0200

    [updated] leads templates

--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     # path('<int:pk>/', lead_detail, name='lead-detail'),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),  # CBV
     # path('<int:pk>/update/', lead_update, name='lead-update'),
-    # path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),  # CBV
-    path('<int:pk>/delete/', lead_delete, name='lead-delete'),
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),  # CBV
+    # path('<int:pk>/delete/', lead_delete, name='lead-delete'),
     path('create/', LeadCreateView.as_view(), name='lead-create'),  # CBV
     # path('create/', lead_create, name='lead-create'),
 
