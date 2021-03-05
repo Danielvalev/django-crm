067a5bbcd44c6c79ce73c82ae2b383c69171ea92
Daniel Valev <danielvalev89@gmail.com>
2021-03-05 11:54:27 +0200

    [added] send email functionality

--- crm/leads/views.py
+++ crm/leads/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -30,6 +31,16 @@ class LeadCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('leads:lead-list')
+
+    def form_valid(self, form):
+        # TODO send email
+        send_mail(
+            subject='A lead has been created',
+            message='Go to the site to see the new lead',
+            from_email='test@test.com',
+            recipient_list=['test2@test.com'],
+        )
+        return super(LeadCreateView, self).form_valid(form)
 
 
 class LeadUpdateView(UpdateView):
